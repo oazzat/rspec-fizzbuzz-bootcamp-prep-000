6576d2389227250ec0ef75cd9a65405d3e2075e7
Omar A <oazzat19@gmail.com>
2018-09-15 23:46:56 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz (int)
+
+if int % 3 == 0
+  return "FIZZ"
+end
+
+elsif int % 5 == 0
+  return "BUZZ"
+end
+
+elsif (int%3==0 && int%5==0)
+  return "FIZZBUZZ"
+end
+
+
+end
